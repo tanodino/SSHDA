839fed5f1f7a627a36837acb9b26707c9bd8fefd
tanodino <dino@Dinos-MacBook-Pro.local>
2024-01-30 14:08:35 +0100

    modif py

--- main.py
+++ main.py
@@ -124,8 +124,8 @@ y_train_source = torch.tensor(source_label, dtype=torch.int64)
 
 import torchvision.transforms as T 
 
-w_size = 128 
-resize = T.Resize((w_size, w_size), interpolation=T.InterpolationMode.BICUBIC)
+#w_size = 128 
+#resize = T.Resize((w_size, w_size), interpolation=T.InterpolationMode.BICUBIC)
 print("\tBEFORE RESIZING ",x_train_source.shape)
 x_train_source = resize(x_train_source)
 print("\tAFTER RESIZING ",x_train_source.shape)
