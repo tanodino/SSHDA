e2728f06d66d9c33fd37bdc3b4a86202e42efea5
tanodino <dino@Dinos-MacBook-Pro.local>
2024-02-01 15:07:43 +0100

    a

--- main.py
+++ main.py
@@ -298,7 +298,7 @@ optimizer = torch.optim.AdamW(params=model.parameters(), lr=learning_rate)
 scl = SupervisedContrastiveLoss()
 
 
-epochs = 300
+epochs = 3#300
 # Loop through the data
 valid_f1 = 0.0
 margin = .3
