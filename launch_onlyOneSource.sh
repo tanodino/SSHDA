d52b9c48c5a9372fe9f21ed34e171ff3c13b5149
tanodino <dino@Dinos-MacBook-Pro.local>
2024-02-11 18:54:40 +0100

    a

--- launch_onlyOneSource.sh
+++ launch_onlyOneSource.sh
@@ -2,6 +2,6 @@ for i in 50 100 150 200 250 300 350 400
 do
     for j in 0 1 2 3 4
     do
-        python main_onlyOneSource $1 $2 $i $j > log_direct_${1}_${2}_${i}_${j}
+        python main_onlyOneSource.py $1 $2 $i $j > log_direct_${1}_${2}_${i}_${j}
     done
 done
