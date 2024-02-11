caa357a8a3f857e5d8ef5318a5009e0f62323498
tanodino <dino@Dinos-MacBook-Pro.local>
2024-02-11 18:28:28 +0100

    a

--- launch_fixMatch.sh
+++ launch_fixMatch.sh
@@ -2,6 +2,6 @@ for i in 50 100 150 200 250 300 350 400
 do
     for j in 0 1 2 3 4
     do
-        echo "python fixmatch.py $1 $2 $i $j"
+        echo "python fixmatch.py $1 $2 $i $j > log_$1_$2_$i_$j"
     done
 done
