b6ab361001a6c902236824c17a7ac4595fa09d8f
tanodino <dino@Dinos-MacBook-Pro.local>
2024-02-13 18:32:42 +0100

    subsamples 25

---
+++ subsamples.py
@@ -0,0 +1,38 @@
+import sys
+import glob
+import numpy as np
+from sklearn.utils import shuffle
+
+
+def getNewTrainIdx(labels, train_idx, val):
+    newTrainIdx = []
+    hashMap = {}
+    for l in np.unique(labels):
+        hashMap[l] = []
+    for v in train_idx:
+        hashMap[labels[i]].append(v)
+    for l in hashMap.keys():
+        temp = shuffle( hashMap[l] )
+        newTrainIdx.append( temp[0:val] )
+    newTrainIdx = np.concatenate(newTrainIdx,axis=0)
+    return newTrainIdx
+        
+
+
+
+dir_name = sys.argv[1]
+#MS_1_300_train_idx.npy
+query_string = "_50_"
+val = 25
+substitute_string = "_%d_"%val
+
+fileNames = glob.glob("%s/*%s*py"%(dir_name,query_string))
+for fName in fileNames:
+    prefix = fName.split("_")[0]
+    labels = np.load("%s/%s_label_filtered.npy"%(dir_name,prefix))
+    train_idx = np.load(fName)
+    newTrainIdx = getNewTrainIdx(labels, train_idx, val)
+    newFileName = fName.replace(query_string, substitute_string)
+    print(newTrainIdx.shape)
+    print("============")
+    #np.save(newFileName, newTrainIdx)
