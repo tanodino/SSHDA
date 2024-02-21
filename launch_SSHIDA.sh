6f756cac787c868252d249a701da9f21d633c07e
Cassio F. Dantas <cassiofragadantas@gmail.com>
2024-02-21 12:32:15 +0000

    Mispelling

--- launch_SSHIDA.sh
+++ launch_SSHIDA.sh
@@ -2,7 +2,7 @@ for i in 25 50 100 200
 do
     for j in 0 1 2 3 4
     do
-        python SSHIDA2.py $1 $2 $3 $i $j > log_HIDA_${1}_${2}_${3}_${i}_${j}
+        python SSHIDA.py $1 $2 $3 $i $j > log_HIDA_${1}_${2}_${3}_${i}_${j}
     done
 done
 
