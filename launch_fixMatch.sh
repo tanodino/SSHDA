e2b59affc71578d93e1649953937ce4e75afb047
tanodino <dino.ienco@irstea.fr>
2024-03-07 10:08:38 +0000

    cleaning

--- launch_fixMatch.sh
+++
@@ -1,9 +0,0 @@
-#for i in 50 100 150 200 250 300 350 400
-for i in 25 50 100 200
-#for i in 25
-do
-    for j in 0 1 2 3 4
-    do
-        python fixmatch.py $1 $2 $i $j > log_fixmatch_${1}_${2}_${i}_${j}
-    done
-done
